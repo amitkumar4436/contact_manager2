e06f93ea8e609271000a32db832fd3a2a497f5d7
Amit Kumar <amit.kumar4436@gmail.com>
2015-06-30 16:21:11 +0530

    Implement validations on person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Person, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
   it 'is invalid without a first name' do
   	person = Person.new(first_name: nil)
-  	expect(person).not_to_be_valid
+  	expect(person).not_to be_valid
   end
 end
